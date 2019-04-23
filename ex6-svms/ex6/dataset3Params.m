8073137a81b6356764524d0a5363a32ee6e245bb
rusheb <rusheb.shah@gmail.com>
2019-04-23 20:43:08 +0100

    6.1.2.3 Example Dataset 3 (Finding optimal parameters using cross validation)

--- ex6-svms/ex6/dataset3Params.m
+++ ex6-svms/ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +23,27 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
-
+%possible_values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+%pred_error = zeros(length(possible_values));
+%for i = 1:length(possible_values)
+%  for j = 1:length(possible_values)
+%    V = [possible_values(i), possible_values(j)];
+%    fprintf("next")
+%    disp(V)
+%    C = possible_values(i);
+%    sigma = possible_values(j);
+%    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+%    predictions = svmPredict(model, Xval);
+%    pred_error(i, j) = mean(double(predictions ~= yval));
+%    disp(pred_error)
+%  end
+%end
+%
+%min_pred_error = min(pred_error(:));
+%[min_i, min_j] = find(pred_error==min_pred_error);
+%
+%C = possible_values(min_i);
+%sigma = possible_values(min_j);
 
 % =========================================================================
 
